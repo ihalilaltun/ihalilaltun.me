8807a7e1c0628a4443f3c9381f65e13dffe68ba1
Ibrahim Halil Altun <ihalilaltun@gmail.com>
2018-09-07 12:36:42 +0300

    Update FrizbitServiceWorker.js

--- FrizbitServiceWorker.js
+++ FrizbitServiceWorker.js
@@ -1,2 +1,2 @@
-importScripts('https://cdn.frizbit.com/frizbitjs/28cbd76d-df79-4244-a4a8-b245e1df8f9a.js');
-//importScripts('https://cdn.segmentify.com/93a9a7c4-dc2e-49f7-832e-7c7fc3dabbf9/sw.js');
+//importScripts('https://cdn.frizbit.com/frizbitjs/28cbd76d-df79-4244-a4a8-b245e1df8f9a.js');
+importScripts('https://cdn.segmentify.com/93a9a7c4-dc2e-49f7-832e-7c7fc3dabbf9/sw.js');

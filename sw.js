4dae0c458428731cf5737d49ed06648679b3fb91
Ibrahim Halil Altun <ihalilaltun@gmail.com>
2018-10-09 12:56:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-importScripts('https://ihalilaltun.me/SegmentifySw.js');
 importScripts('https://www.gstatic.com/firebasejs/5.5.3/firebase.js');
+importScripts('https://ihalilaltun.me/SegmentifySw.js');
+
 //importScripts('https://ihalilaltun.me/OtherSw.js');
 
 /*

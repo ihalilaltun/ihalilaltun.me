04895618c6833734225e1c4fb0f9f7b8799384a8
Ibrahim Halil Altun <ihalilaltun@gmail.com>
2018-10-09 14:14:14 +0300

    Update SegmentifySw.js

--- SegmentifySw.js
+++ SegmentifySw.js
@@ -1,48 +1,11 @@
 'use strict';
 
-var messaging = firebase.messaging();
-
 var defaults = {
   icon: 'https://cdn.segmentify.com/push/error.png',
   restUrl: 'https://dce-test.segmentify.com/',
   errorTitle: 'Notification Failed',
   errorMessage: 'Can\'t show the push notification due to possible network problem.'
 };
-
-
-messaging.getToken().then(function(currentToken) {
-  if (currentToken) {
-    sendTokenToServer(currentToken);
-    updateUIForPushEnabled(currentToken);
-    console.log(currentToken);
-  } else {
-    // Show permission request.
-    console.log('No Instance ID token available. Request permission to generate one.');
-    // Show permission UI.
-    updateUIForPushPermissionRequired();
-    setTokenSentToServer(false);
-  }
-}).catch(function(err) {
-  console.log('An error occurred while retrieving token. ', err);
-  showToken('Error retrieving Instance ID token. ', err);
-  setTokenSentToServer(false);
-});
-
-messaging.onTokenRefresh(function() {
-  messaging.getToken().then(function(refreshedToken) {
-    console.log('Token refreshed.');
-    // Indicate that the new Instance ID token has not yet been sent to the
-    // app server.
-    setTokenSentToServer(false);
-    // Send Instance ID token to app server.
-    sendTokenToServer(refreshedToken);
-    // ...
-  }).catch(function(err) {
-    console.log('Unable to retrieve refreshed token ', err);
-    showToken('Unable to retrieve refreshed token ', err);
-  });
-});
-
 
 self.addEventListener('install', function (event) {
   self.skipWaiting();

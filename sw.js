ae64de28840b03ad91a9e2fe29eb90d490c8d32d
Ibrahim Halil Altun <ihalilaltun@gmail.com>
2019-02-15 14:32:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 var currentDate = new Date().toJSON().slice(0,10).replace(/-/g,'');
-importScripts("https://ihalilaltun.me/SegmentifySw.js");
+importScripts("https://ihalilaltun.me/SegmentifySw.js?v1");

9b739c23ea6b05ed0d15c4e7ec5dc6cf3eabcbfd
Ergin EROĞLU <eeroglu@gmail.com>
2020-08-24 18:23:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-var url = 'https://cdn.segmentify.com/v3/dev/sw.debug.js?v' + Math.round(Math.random() * 5000);
+const url = 'https://cdn.segmentify.com/v3/dev/sw.debug.js?v' + Math.round(Math.random() * 5000);
 importScripts(url);
 //importScripts('https://cdn.segmentify.com/v3/dev/sw.debug.js?v4');

c6a16986183dde30f344031ed267002258499a2d
Ergin EROĞLU <eeroglu@gmail.com>
2020-08-25 09:36:36 +0300

    Update sw_root.js

--- sw_root.js
+++ sw_root.js
@@ -1,1 +1,7 @@
-importScripts("https://ihalilaltun.me/SegmentifySw.js");
+const url = 'https://ihalilaltun.me/SegmentifySw.js';
+
+self.addEventListener('error', function(e) {
+  console.log(e.filename, e.lineno, e.colno, e.message, JSON.stringify(e.error));
+});
+
+importScripts(url);

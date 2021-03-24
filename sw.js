8747e1edb328d1d03f0c8049ae03b5aa6d39eceb
Ibrahim Halil Altun <ihalilaltun@gmail.com>
2021-03-24 10:53:36 +0300

    fix

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-importScripts('https://ihalilaltun.me/custom_sw.js');
+importScripts('https://ihalilaltun.me/custom_sw.js?1');
 updateRegistration('ae272bfb-214b-4cdd-b5c4-1dddde09e95e', 'https://gimli-dev.segmentify.com/');
